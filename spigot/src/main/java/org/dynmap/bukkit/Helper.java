80f6b16fb314506dc026f7a3c19b524ac7810b6c
woundedkoba <woundedkoba@protonmail.com>
2026-02-15 10:41:16 -0500

    MC 1.21.11 Support
    Added a catch for v1.21.12 support in case it works as is per:
    [Commit d8bc15d by mikeprimm](https://github.com/webbukkit/dynmap/commit/d8bc15d64555b8be9c6c4abb24c0873a75fc74d6)

--- spigot/src/main/java/org/dynmap/bukkit/Helper.java
+++ spigot/src/main/java/org/dynmap/bukkit/Helper.java
@@ -40,6 +40,9 @@ public class Helper {
                 Log.info("Loading Glowstone support");
                 BukkitVersionHelper.helper = loadVersionHelper("org.dynmap.bukkit.helper.BukkitVersionHelperGlowstone");
             }
+            else if (v.contains("(MC: 1.21.11")) {
+                BukkitVersionHelper.helper = loadVersionHelper("org.dynmap.bukkit.helper.v121_7.BukkitVersionHelperSpigot121_7");
+            }
             else if (v.contains("(MC: 1.21.7")) {
                 BukkitVersionHelper.helper = loadVersionHelper("org.dynmap.bukkit.helper.v121_7.BukkitVersionHelperSpigot121_7");
             }
@@ -57,6 +60,9 @@ public class Helper {
             }
             else if (v.contains("(MC: 1.21)") || v.contains("(MC: 1.21.1)")) {
                 BukkitVersionHelper.helper = loadVersionHelper("org.dynmap.bukkit.helper.v121.BukkitVersionHelperSpigot121");
+            }
+            else if (v.contains("(MC: 1.21.")) { // Set up in case 1.21.12 works 'as is'
+                BukkitVersionHelper.helper = loadVersionHelper("org.dynmap.bukkit.helper.v121_7.BukkitVersionHelperSpigot121_7");
             }
             else if (v.contains("(MC: 1.20)") || v.contains("(MC: 1.20.1)")) {
             	BukkitVersionHelper.helper = loadVersionHelper("org.dynmap.bukkit.helper.v120.BukkitVersionHelperSpigot120");
